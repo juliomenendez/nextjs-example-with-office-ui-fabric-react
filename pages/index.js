c84b7fc9b45a4af9260dcdb6ded82543b7ec53f9
Julio Menendez <hello@juliomenendez.com>
2019-01-29 21:57:01 -0800

    Initial commit.

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Fabric } from 'office-ui-fabric-react/lib-commonjs/Fabric';
+import { DefaultButton } from 'office-ui-fabric-react/lib-commonjs/Button';
+
+export default () => (
+    <Fabric>
+        Welcome to next.js!
+        <div><DefaultButton>A button</DefaultButton></div>
+    </Fabric>
+);
